47ca8419127bfe74f2eaffce6298b7452d784e77
Pontus Asp <pontus.asp@gmail.com>
2021-05-05 14:31:50 +0200

    added favicon and made background gradient

--- src/App.js
+++ src/App.js
@@ -11,13 +11,23 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-content">
-          <p className="App-bigtext">
-            Hello! My name is <span className="My-name">Pontus Asp</span>.
-          </p>
-          <p className="App-slim text-i">
-            I am a student in the <span className="My-programme">Information and Communication Technology</span> programme
-            at <span className="My-school">KTH Royal Institute of Technology</span> in Stockholm.
-          </p>
+          <div className="App-page">
+            <p className="App-bigtext">
+              Hello! My name is <span className="My-name">Pontus Asp</span>.
+            </p>
+            <p className="App-slim text-i">
+              I am a student in the <span className="My-programme">Information and Communication Technology</span> programme
+              at <span className="My-school">KTH Royal Institute of Technology</span> in Stockholm.
+            </p>
+            <div>
+              
+            </div>
+          </div>
+          <div className="App-page">
+            <p>
+              Test
+            </p>
+          </div>
         </div>
         <header className="App-footer">
           <div className="App-logo">P/A</div>
