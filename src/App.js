fd29d1743b27848168145c0a2b9f406a67ba6a04
Pontus Asp <pontus.asp@gmail.com>
2021-07-27 09:12:13 +0200

    changed from single-page to multi-page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import logo from './pontusasp-logo.svg';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
 
@@ -11,36 +12,84 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-content">
-          <div className="App-page">
-            <p className="App-bigtext">
-              Hello! My name is <span className="My-name">Pontus Asp</span>.
-            </p>
-            <p className="App-slim text-i">
-              I am a student at the <span className="My-programme">Information and Communication Technology</span> programme
-              at <span className="My-school">KTH Royal Institute of Technology</span> in Stockholm.
-            </p>
-            <div>
-              
+        <Router>
+          <div className="App-content">
+            <Switch>
+              <Route path="/Apps/the-red-painter">
+                <TheRedPainter />
+              </Route>
+              <Route path="/Apps">
+                <Apps />
+              </Route>
+              <Route path="/Links">
+                <Links />
+              </Route>
+              <Route path="/">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+          <footer className="App-footer">
+            <div className="App-menu">
+            <Link to="/"><img src={logo} className="App-logo" alt="App-logo" /></Link>
+              <div><Link to="/Apps">Apps</Link></div>
+              <div><Link to="/Links">Links</Link></div>
             </div>
-          </div>
-          <div className="App-page">
-            <p>
-              Test
-            </p>
-          </div>
-        </div>
-        <footer className="App-footer">
-          <div className="App-menu">
-          <img src={logo} className="App-logo" alt="App-logo" />
-            <div>Apps</div>
-            <div>Links</div>
-            <div>About</div>
-          </div>
-        </footer>
+          </footer>
+        </Router>
       </div>
     );
   }
+}
+
+function About() {
+  return (
+    <div id="About" className="App-page">
+      <p className="App-bigtext">
+        Hello! My name is <span className="My-name">Pontus Asp</span>.
+      </p>
+      <p className="App-slim text-i">
+        I am a student at the <span className="My-programme">Information and Communication Technology</span> programme
+        at <span className="My-school">KTH Royal Institute of Technology</span> in Stockholm, Sweden.
+      </p>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+function Apps() {
+  return (
+    <div id="Apps" className="App-page">
+      <p>
+      <Link to="/Apps/the-red-painter">The Red Painter</Link>
+      </p>
+    </div>
+  );
+}
+
+function Links() {
+  return (
+    <div id="Links" className="App-page">
+      <p>
+        Test
+      </p>
+    </div>
+  );
+}
+
+function TheRedPainter() {
+  return (
+    <div className="App-page">
+      <p className="App-bigtext">
+        The Red Painter
+      </p>
+      <p className="App-slim text-i">
+        <Link to="/RedPainter">Privacy Policy</Link>
+      </p>
+    </div>
+  );
 }
 
 export default App;
